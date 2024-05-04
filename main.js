1d15eeb5c8dedb0027bc6220dc2a9f8a94dd35ed
Syahril Fitrawan Abadi <syahrilfitrawanabadi@gmail.com>
2024-05-04 23:25:46 +0800

    lanjut angel

--- main.js
+++ main.js
@@ -17,36 +17,7 @@ if (close) {
 
 // SLIDER 
 
-const imageWrapper = document.querySelector('.image-wrapper')
-const imageItems = document.querySelectorAll('.image-wrapper > *')
-const imageLength = imageItems.length
-const perView = 2
-let totalScroll = 0
-const delay = 2000
-
-imageWrapper.style.setProperty('--per-view', perView)
-for (let i = 0; i < perView; i++) {
-    imageWrapper.insertAdjacentHTML('beforeend', imageItems[i].outerHTML)
-}
-
-let autoScroll = setInterval(scrolling, delay)
-
-function scrolling() {
-    totalScroll++
-    if (totalScroll == imageLength + 1) {
-        clearInterval(autoScroll)
-        totalScroll = 1
-        imageWrapper.style.transition = '0s'
-        imageWrapper.style.left = '0'
-        autoScroll = setInterval(scrolling, delay)
-    }
-    const widthEl = document.querySelector('.image-wrapper > :first-child').offsetWidth + 24
-    imageWrapper.style.left = `-${totalScroll * widthEl}px`
-    imageWrapper.style.transition = '.3s'
-}
-
 // SLIDER 2
-
 const imgList = document.querySelector('.slider-wrapper .image-list');
 const slideButtons = document.querySelectorAll('.slider-wrapper .slide-button');
 const maxScrollLeft = imgList.scrollWidth - imgList.clientWidth;
